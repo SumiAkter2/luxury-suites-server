698a519758647121f24ab7b975db000b2e67acd5
SumiAkter2 <akteralisha36@gmail.com>
2022-08-28 20:53:07 +0600

    added env and mongodb connect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,6 +6,18 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c2jlaov.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("connected");
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Luxury suites");
